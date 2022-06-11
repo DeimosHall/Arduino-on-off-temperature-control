d0d16c8370aec15e583c1575f5c1417f586c5da0
Deimos <deimoshall@gmail.com>
2022-06-10 19:13:21 -0500

    Control working with fan

--- Arduino-on-off-temperature-control.ino
+++ Arduino-on-off-temperature-control.ino
@@ -1,32 +1,41 @@
 #define PIN_TEMP_SENSOR A1
-#define FAN 3
+#define PIN_FAN 3
 
-int setPoint = 50;
+int setPoint = 60;
 int upGraphLimit = setPoint + 10, lowGraphLimit = setPoint - 10;
-float temperature, lowerLimit = setPoint - 2, upperLimit = setPoint + 2;
+float temperature, lowerLimit = setPoint - 1, upperLimit = setPoint + 0;
+float error;
 
 void setup() {
   Serial.begin(9600);
   //Serial.println("Conection started");
-  pinMode(FAN, OUTPUT);
+  pinMode(PIN_FAN, OUTPUT);
 }
 
 void loop() {
   temperature = ((float(analogRead(PIN_TEMP_SENSOR)) * 5 / 1023) / 0.01) - 9;
+  error = setPoint - temperature;
+  
   Serial.print("Up limit: ");
   Serial.print(upGraphLimit);
   Serial.print(",");
+  Serial.print("SetPoint: ");
+  Serial.print(setPoint);
+  Serial.print(",");
+  
   Serial.print("Temperature: ");
   Serial.print(temperature);
+  
   Serial.print(",");
   Serial.print("Low limit: ");
   Serial.println(lowGraphLimit);
+  
 
   if (temperature > upperLimit) {
-    digitalWrite(FAN,HIGH);
+    digitalWrite(PIN_FAN,HIGH);
   } else if (temperature < lowerLimit) {
-    digitalWrite(FAN,LOW);
+    digitalWrite(PIN_FAN,LOW);
   }
 
-  delay(100);
+  delay(1000);
 }
